b86d5403175d51441cfd2beb4654d8b2421a7c9b
Moein.MAC <moein.mac@gmail.com>
2023-11-07 14:06:53 +0330

    board and draw

---
+++ js/board.js
@@ -0,0 +1,12 @@
+const boardElement = document.querySelector(".board");
+const tiles = Array.from(document.querySelectorAll(".tile"));
+
+const Board = {
+  tiles,
+  draw(gameState) {
+    gameState.split("").forEach((item,index) =>{
+        tiles[index].style.order = item
+        console.log({item,index});
+    })
+  },
+};
